63d23c80f020fa861930dcc04a20c231b57e354f
lampeduza <lmdogg@mail.ru>
2020-09-17 22:20:07 +0700

    webP

--- gulpfile.js
+++ gulpfile.js
@@ -79,7 +79,7 @@ gulp.task("images", function() {
 
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 75}))
+    .pipe(webp({quality: 80}))
     .pipe(gulp.dest("build/img"));
 });
 
